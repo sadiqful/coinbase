9dbd38b8a639514708d40d1633e1e60e55db9891
Aliyu <sadiqful@gmail.com>
2023-06-12 13:45:44 +0100

    button style

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -21,3 +21,24 @@ export default function Button({ text, icon = false, subduedButton = false }) {
   );
 }
 
+const Div = styled.div`
+  button {
+    padding: 0.7rem 1.3rem;
+    background-color: var(--blue);
+    color: white;
+    border: none;
+    border-radius: 0.5rem;
+    font-size: 1.1rem;
+    font-weight: bolder;
+    cursor: pointer;
+  }
+  .subduedBtn {
+    background-color: var(--subdue-button);
+  }
+  @media screen and (min-width: 280px) and (max-width: 1080px) {
+    flex-direction: row;
+    button {
+      flex-direction: row;
+    }
+  }
+`;
