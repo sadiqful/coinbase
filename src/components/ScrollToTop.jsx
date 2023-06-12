df7568f1091ff02a5bbc3f510359d0b7055acdf1
Aliyu <sadiqful@gmail.com>
2023-06-12 13:51:24 +0100

    scroll structure

---
+++ src/components/ScrollToTop.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { FaChevronUp } from "react-icons/fa";
+function ScrollToTop() {
+  const [visible, setVisible] = useState(false);
+  window.addEventListener("scroll", () => {
+    window.pageYOffset > 100 ? setVisible(true) : setVisible(false);
+  });
+
+  return (
+    <Div>
+      <a href="#" className={`${visible ? "block" : "none"}`}>
+        <FaChevronUp />
+      </a>
+    </Div>
+  );
+}
+
